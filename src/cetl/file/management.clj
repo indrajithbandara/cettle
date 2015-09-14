1a70ecd78219098e1ec3bb683cf9e39b25d2bd67
Geem7n <gadebesin@gmail.com>
2015-09-14 21:13:08 +0100

    sub dirs implementation for file list func

--- src/cetl/file/management.clj
+++ src/cetl/file/management.clj
@@ -16,65 +16,63 @@
 ;1. List files in directory
 ;2. Put each path onto a channel
 ;3. Pass each path to any input component
-;TODO implement include-hidden-files? for files only
 ;TODO move zip command and gzip command into parent func
-
 (defn cetl-file-list
   [path & {:keys
-           [dirs-only? files-only? files-only? dirs-&-files?
-            include-sub-dirs?]}]
+           [dirs-only? files-only? dirs-&-files?
+            include-sub-dirs? include-hidden-files?]}]
   (letfn
     [(dirs-&-files
        [p]
        (clojure.string/split
          (get (clojure.java.shell/sh
                 "sh" "-c"
-                (str " cd " p ";" "  ls -d $PWD/*/ " ";" " ls -d -1 $PWD/*.* "))
+                (str " cd " p ";"
+                     " find `pwd` -maxdepth 1 "))
               :out) #"\n"))
      (files-only
        [p]
        (clojure.string/split
          (get (clojure.java.shell/sh
                 "sh" "-c"
-                (str " cd " p ";" " ls -d -1 $PWD/*.* "))
+                (str " cd " p ";"
+                     " find `pwd` -type f -maxdepth 1 "))
               :out) #"\n"))
      (dirs-only
        [p]
        (clojure.string/split
          (get (clojure.java.shell/sh
                 "sh" "-c"
-                (str " cd " p ";" " ls -d -1 $PWD/*/ "))
+                (str " cd " p ";"
+                     " find `pwd` -type d -maxdepth 1 "))
               :out) #"\n"))
      (include-dirs-only-sub-dirs
        [p]
-       (map #(str % "/")
-         (clojure.string/split
-           (get (clojure.java.shell/sh
-                  "sh" "-c"
-                  (str " cd " p ";" " find `pwd` -type d "))
-                :out) #"\n")))
-     (include-files-only-sub-dirs
-       [p]
-       (mapv #(.getPath %)
-            (filter #(if (.isFile %) (.getPath %))
-                    (file-seq (File. p)))))]
+       (clojure.string/split
+         (get (clojure.java.shell/sh
+                "sh" "-c"
+                (str " cd " p ";"
+                     " find `pwd` -type d "))
+              :out) #"\n"))]
     (cond
       (and (= dirs-&-files? true)
-           (= include-sub-dirs? false))
+           (= include-sub-dirs? false)
+           (= include-hidden-files? false))
       (dirs-&-files path)
       (and (= files-only? true)
-           (= include-sub-dirs? false))
+           (= include-sub-dirs? false)
+           (= include-hidden-files? false))
       (files-only path)
       (and (= dirs-only? true)
-           (= include-sub-dirs? false))
+           (= include-sub-dirs? false)
+           (= include-hidden-files? false))
       (dirs-only path)
       (and (= dirs-only? true)
-           (= include-sub-dirs? true))
-      (include-dirs-only-sub-dirs path)
-      (and (= files-only? true)
-           (= include-sub-dirs? true))
-      (include-files-only-sub-dirs path))))
+           (= include-sub-dirs? true)
+           (= include-hidden-files? false))
+      (include-dirs-only-sub-dirs path))))
 
+;find `pwd` -type f -not -path '*/\.*'
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn zip-command
@@ -108,3 +106,7 @@
 
 #_(defn cetl-file-unarchive
   [path in out & {:keys []}])
+(if (= include-hidden-files? true)
+  (mapv #(.getPath %)
+        (filter #(if (.isFile %) (.getPath %))
+                (file-seq (File. p)))))
