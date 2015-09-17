6c5a536640fdb79c97c73203b09c0d342c6e4c6d
Geem7n <gadebesin@gmail.com>
2015-09-17 22:24:04 +0100

    implemented file encoding function

--- src/cetl/file/management.clj
+++ src/cetl/file/management.clj
@@ -1,7 +1,7 @@
 (ns cetl.file.management
-  (:require [clojure.string :as s]
-            [cetl.utils.component-utils :as c-utils])
-  (:import (java.io File FileFilter)))
+  (:require [clojure.string :as s])
+  (:import (org.apache.commons.io FileUtils)
+           (java.io File)))
 
 (use '[clojure.java.shell :only [sh]])
 
@@ -63,7 +63,11 @@
            (= include-hidden-files? false))
       (include-dirs-only-sub-dirs path))))
 
-;find `pwd` -type f -not -path '*/\.*'
+
+
+#_(defn cetl-file-unarchive
+    [path in out & {:keys []}])
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn cetl-file-archive
@@ -73,23 +77,53 @@
        [p]
        (clojure.java.shell/sh
          "sh" "-c"
-          (str " cd "  (.getParent (File. p)) "/;"
-               " zip " (last (s/split p #"/")) ".zip"
-               " -r "  (last (s/split p #"/")))))
+         (str " cd " (.getParent (File. p)) "/;"
+              " zip " (last (s/split p #"/")) ".zip"
+              " -r " (last (s/split p #"/")))))
      (gzip
        [p]
        (clojure.java.shell/sh
          "sh" "-c"
-          (str " cd " (.getParent (File. p)) "/;"
-               " tar -cvzf " (last (s/split p #"/")) ".tar.gz"
-               " " (last (s/split p #"/")))))]
+         (str " cd " (.getParent (File. p)) "/;"
+              " tar -cvzf " (last (s/split p #"/")) ".tar.gz"
+              " " (last (s/split p #"/")))))]
     (cond
       (= archive-format :zip) (zip path)
       (= archive-format :gzip) (gzip path))))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defn cetl-change-file-encoding
+  [in-path out-path & {:keys [from-encode to-encode]}]
+  (letfn [(encoding
+            [in out x xs]
+            (FileUtils/write
+              (File. out)
+              (FileUtils/readFileToString
+                (File. in) x) xs))]
+    (cond (and (= from-encode :UTF-8)
+               (= to-encode :ISO-8859-15))
+          (encoding in-path out-path "UTF-8" "ISO-8859-15")
+          (and (= from-encode :ISO-8859-15)
+               (= to-encode :UTF-8))
+          (encoding in-path out-path "ISO-8859-15" "UTF-8"))))
 
 
-#_(defn cetl-file-unarchive
-  [path in out & {:keys []}])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
