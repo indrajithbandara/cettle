2a36a77b95c024ec9ff408674a7246b7c2b1204c
Geem7n <gadebesin@gmail.com>
2015-10-29 20:56:04 +0000

    adding error handling

--- test/cetl/core_test.clj
+++ test/cetl/core_test.clj
@@ -3,16 +3,47 @@
             [cetl.core :refer :all]
             [cetl.file.management :refer :all]))
 
-(deftest a-test
-  (testing "Components test"
-    (is (map? (cetl-file-archive )))))
 
 
-;TODO test why "/Users/gregadebesin/Development/Cetl/TestFiles copy" a path with a space in name dosent work
+(cetl-list-file {:path "/Users/gregadebesin/Development"
+                 :exec :list-files})
 
-(def list-files (cetl-file-list {:path "/Users/gregadebesin/Development/Cetl "
-                                 :list :dirs}))
+(cetl-list-file {:path "/Users/gregadebesin/Development"
+                 :exec :list-dirs-files})
 
-(map (fn [x] (cetl-file-archive {:path x :archive :zip})) list-files)
+(cetl-list-file {:path "/Users/gregadebesin/Development"
+                 :exec :list-dirs-sub-dirs})
 
-; file -I test-file.txt
+(cetl-list-file {:path "/Users/gregadebesin/Development"
+                 :exec :list-dirs})
+
+;=====================================================================
+
+(cetl-archive-file {:path "/Users/gregadebesin/Development/Cetl"
+                    :file "archive"
+                    :exec :zip-file})
+
+(cetl-archive-file {:path "/Users/gregadebesin/Development/Cetl"
+                    :file "archive"
+                    :exec :gzip-file})
+
+(cetl-create-temp-file {:path "/Users/gregadebesin/Development"
+                        :file "foo.tmp"
+                        :exec :create-temp-file})
+
+(cetl-copy-file {:in-path "/Users/gregadebesin/Development"
+                 :out-path "/Users/gregadebesin"
+                 :file "foo.tmp"
+                 :exec :copy-file})
+
+(cetl-delete-file {:path "/Users/gregadebesin/Development"
+                   :file "foo.tmp"
+                   :exec :delete-file})
+
+(cetl-file-properties {:path "/Users/gregadebesin/Development/datomic-code.txt"
+                       :exec :file-properties})
+
+
+
+
+
