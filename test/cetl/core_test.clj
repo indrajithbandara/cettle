95fb38e0a1368a7c95b5faa1b0df0bd10726fb38
adebesin <gregory.adebesin@gmail.com>
2015-12-05 21:49:06 +0000

    Transition to protocols

--- test/cetl/core_test.clj
+++ test/cetl/core_test.clj
@@ -1,52 +1,18 @@
 (ns cetl.core-test
   (:require [clojure.test :refer :all]
             [cetl.core :refer :all]
-            [cetl.file.management :refer :all]))
+            [clojure.java.io :as io]
+            [cetl.file.management :refer :all]
+            [cetl.utils.component-utils :refer [file-exists? dir-exists?]]))
 
-(cetl-file-management {:path "/Users/gregadebesin/Development/Cetl"
-                    :file "foo"
-                    :exec :zip-file})
+(-> {:path "/Users/gra11/Development"
+     :file "file.txt"
+     :exec :count-row-file}
+    (cetl-file-management))
 
-(cetl-file-management {:path "/Users/gregadebesin/Development/Cetl"
-                    :file "foo"
-                    :exec :gzip-file})
-
-(cetl-file-management {:path "/Users/gregadebesin/Development"
-                        :file "foo.tmp"
-                        :exec :create-temp-file})
-
-(cetl-file-management {:in-path "/Users/gregadebesin/Development"
-                 :out-path "/Users/gregadebesin"
-                 :file "foo.tmp"
-                 :exec :copy-file})
-
-(cetl-file-management {:path "/Users/gregadebesin/Development"
-                   :file "untitled.txt"
-                   :exec :delete-file})
-
-(cetl-file-management {:path "/Users/gregadebesin/Development"
-                       :file "foo.tmp"
-                       :exec :properties-file})
-
-(cetl-file-management {:path "/Users/gregadebesin/Development/untitled.txt"
-                   :exec :ISO-8859-1})
-
-
-(cetl-file-management {:file-one "foo.txt"
-                    :path-one "/Users/gregadebesin/Development"
-                    :file-two "bar.txt"
-                    :path-two "/Users/gregadebesin/Development"
-                    :exec :compare-file})
-
-(cetl-file-management {:file "tennis.txt"
-                      :path "/Users/gregadebesin/Development"
-                      :exec :count-row-file})
-
-(cetl-file-management {:file "foo.txt"
-                  :path "/Users/gregadebesin/Development"
-                  :exec :touch-file})
-  
-  (cetl-file-management {:file "bar.txt"
-                        :path "/Users/gregadeesin/Development"
-                        :recipient "Gregory"
-                        :exec :gpg-encrypt-file})
+(-> {:path "/Users/gra11/Development"
+     :file "file.txt"}
+    (->file-management)
+    (zip-file)
+    (->file-management)
+    (gzip-file))
