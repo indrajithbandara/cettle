e18ef7cafcc5c1474f81e8e68b6c9a03db1e15ad
adebesin <gregory.adebesin@gmail.com>
2016-02-14 12:37:07 +0000

    Added specter

--- src/cetl/shell/command/archive.clj
+++ src/cetl/shell/command/archive.clj
@@ -1,48 +1,19 @@
 (ns cetl.shell.command.archive
   (:require [clojure.java.io :as io]
             [clojure.string :as s]
-            [cetl.utils.file :refer [parent-path file-name]]))
+            [cetl.utils.file :refer [parent-path file-name]]
+            [cetl.shell.sh :refer [exec-command exec-commands]]
+            [cetl.utils.file :refer [file-name]]))
 
+(use 'com.rpl.specter)
 
 (defrecord CompressionFormat [zip gzip])
 
 (defn compress-command
-  [f]
-  (CompressionFormat. (str " zip " f ".zip -r " f)
-                      (str " tar -cvzf " f ".tar.gz " f)))
-
-#_(defn zip
-  [l]
-  (into {} (map (fn [s]
-                  (exec
-                    "sh" "-c"
-                    (str " cd " (parent-path (io/file s)) ";"
-                         " zip " (file-name (io/file s)) ".zip"
-                         " -r " (file-name (io/file s))))) l)))
-
-(def )
-
-(defn zip
   [s]
-  (exec
-    "sh" "-c"
-    (str " cd " (parent-path (io/file s)) ";"
-         " zip " (file-name (io/file s)) ".zip"
-         " -r " (file-name (io/file s)))))
+  (CompressionFormat. (str " zip " s ".zip -r " s)
+                      (str " tar -cvzf " s ".tar.gz " s)))
 
-(defn gzip
-   [l] (into {} (map (fn [s]
-                       (exec
-                         "sh" "-c"
-                         (str " cd " (parent-path (io/file s)) ";" " tar -cvzf " (file-name (io/file s)) ".tar.gz " (file-name (io/file s))))) l)))
-
-(defn zip-map
-  [f l]
-  (assoc (empty f) :out (map #(str % ".zip") l) :exec :cetl-zip-file))
-
-(defn gzip-map
-  [f l]
-  (assoc (empty f) :out (map #(str % ".tar.gz") l) :exec :cetl-gzip-file))
-
-
-
+(defn cetl-compress
+  [k f x]
+  (transform [ALL] (exec-command (k (compress-command f))) x))
--- src/cetl/shell/command/path.clj
+++ src/cetl/shell/command/path.clj
@@ -1,11 +1,20 @@
 (ns cetl.shell.command.path
   (:require [clojure.string :as s]
-            [cetl.shell.sh :refer [exec]]))
+            [cetl.shell.sh :refer [exec-command]]))
+
+(use 'com.rpl.specter)
 
 (defrecord FilePath [file dir file-dir dir-subdir])
 
-(def path-command (FilePath. " find `pwd ` -type f -maxdepth 1 "
-                             " find `pwd` -type d -not -path '*/\\.*' -maxdepth 1 "
-                             " find `pwd` -maxdepth 1 "
-                             " find `pwd` -type d "))
+(def path-command
+  (FilePath. " find `pwd ` -type f -maxdepth 1 "
+             " find `pwd` -type d -not -path '*/\\.*' -maxdepth 1 "
+             " find `pwd` -maxdepth 1 "
+             " find `pwd` -type d "))
+
+(defn cetl-list
+  [k x]
+  (transform [ALL] (exec-command (k path-command)) x))
+
+
 
